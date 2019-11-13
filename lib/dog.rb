a27edafdf7377b83d97316ae3f3429125b1d3933
Vanna Winland <vgwinland@gmail.com>
2019-11-13 00:13:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -84,9 +84,9 @@ class Dog
         dog
       end
 
-      def update 
-        sql = <<-SQL 
-          UPDATE dogs SET name = ?, breed = ? 
+      def update
+        sql = <<-SQL
+          UPDATE dogs SET name = ?, breed = ?
           WHERE id = ?
           SQL
           DB[:conn].execute(sql, self.name, self.breed, self.id)
