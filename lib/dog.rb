31f14eeeea515d65c8841d84555e7fc0155c976e
Vanna Winland <vgwinland@gmail.com>
2019-11-13 00:02:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,5 +62,16 @@ class Dog
         end.first
       end
 
+      def self.find_by_id(id)
+        sql = <<-SQL 
+          SELECT * FROM dogs 
+          WHERE id = ? 
+          LIMIT 1
+          SQL
+        DB[:conn].execute(sql, id).map do |row|
+          SELF.new_from_db(row)
+        end.first
+      end 
+
 
 end
